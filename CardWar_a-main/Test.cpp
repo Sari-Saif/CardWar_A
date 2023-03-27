e9ba4f5c151b7a0969b2f06f2ea72efce406828f
sari <sygw6457@gmail.com>
2023-03-27 18:51:44 +0300

    working

--- CardWar_a-main/Test.cpp
+++ CardWar_a-main/Test.cpp
@@ -1,5 +1,26 @@
-//
-// Created by sari on 3/27/23.
-//
+/**
+ * implementation file for the TESTING
+ * @author Sari Saif
+ * @since 2023-03-27
+ */
 
-#include "Test.h"
+/**
+ *
+ */
+#include <iostream>
+#include <stdexcept>
+#include "doctest.h"
+#include "sources/player.hpp"
+#include "sources/game.hpp"
+
+use namespace std;
+use namespace ariel;
+
+TEST_CASE("Test case 1"){
+
+    Player p("Sari");
+    Player  p1("Sami");
+    Game game(p,p1);
+    game.playTurn();
+
+}
