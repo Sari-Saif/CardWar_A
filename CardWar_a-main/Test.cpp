b80acf8e72226d03d83e78834fe96a508621ac2f
97253 <sygw6457@gmail.com>
2023-03-28 17:38:39 +0300

    i think it done !

--- CardWar_a-main/Test.cpp
+++ CardWar_a-main/Test.cpp
@@ -19,53 +19,51 @@ using namespace ariel;
 TEST_CASE("Safe Players")
 {
     Player p("Sari");
-    Player  p1("Sami");
-    CHECK(p.stacksize()==26);//trueeee
-    CHECK(p.stacksize()<0);//false
-    CHECK(p.stacksize()== p1.stacksize());//draw
-    CHECK(p.stacksize()+p.cardesTaken()>2*p.stacksize());///isnot safe
-    CHECK(p1.cardesTaken()+p.cardesTaken()==26);//draw
+    Player p1("Sami");
+    CHECK(p.stacksize() == 26);                                 // trueeee
+    CHECK_FALSE(p.stacksize() < 0);                             // false
+    CHECK(p.stacksize() == p1.stacksize());                     // draw
+    CHECK(p.stacksize() + p.cardesTaken() > 2 * p.stacksize()); /// isnot safe
+    CHECK(p1.cardesTaken() + p.cardesTaken() == 26);            // draw
 }
 TEST_CASE("Counstructor checking")
 {
     Player p("Sari");
-    Player  p1("Sami");
-    Game game(p,p1);
-    CHECK_NOTHROW(Game{p,p1});
-    CHECK(p.stacksize()==26);
-    CHECK(p1.stacksize()==26);
-        
-
-
+    Player p1("Sami");
+    Game game(p, p1);
+    CHECK_NOTHROW(Game{p, p1});
+    CHECK_THROWS(Game(p, p));
+    CHECK_THROWS(Player("")); // empty and its not right !
+    CHECK(p.stacksize() == 26);
+    CHECK(p1.stacksize() == 26);
 }
 TEST_CASE("Simoulation 1 ")
 {
     Player p("Sari");
-    Player  p1("Sami");
-    Game game(p,p1);
-    CHECK_NOTHROW(Game{p,p1});
+    Player p1("Sami");
+    Game game(p, p1);
+    CHECK_NOTHROW(Game{p, p1});
     int comption = 10;
-    while(comption>0){
+    while (comption > 0)
+    {
         p.playTurn();
         comption--;
-        bool pAlive = p.stacksize()>21;
-        bool p1_WIN = (p1.stacksize()+p1.cardesTaken()>26);
-        CHECK(p1.stacksize()+p.stacksize()==52);
-        
-        if(pAlive)
+        bool pAlive = p.stacksize() > 21;
+        bool p1_WIN = (p1.stacksize() + p1.cardesTaken() > 26);
+        CHECK(p1.stacksize() + p.stacksize() == 52);
+
+        if (pAlive)
         {
-            int after_round = p.cardesTaken()+p.stacksize();
-            CHECK(p1.stacksize()-after_round<0);// maybe yes or not 
-        
+            int after_round = p.cardesTaken() + p.stacksize();
+            CHECK(p1.stacksize() - after_round < 0); // maybe yes or not
         }
-        else if(p1_WIN)
+        else if (p1_WIN)
         {
-            CHECK(p1.stacksize()>p.stacksize());// true
+            CHECK(p1.stacksize() > p.stacksize()); // true
         }
-       else
-       {
-        CHECK((p1.stacksize()==p.stacksize()) ||(p1.cardesTaken()==p.cardesTaken()));// draww or p is the winner on current round
-       }
+        else
+        {
+            CHECK((p1.stacksize() == p.stacksize()) || (p1.cardesTaken() == p.cardesTaken())); // draww or p is the winner on current round
+        }
+    }
 }
-
-
